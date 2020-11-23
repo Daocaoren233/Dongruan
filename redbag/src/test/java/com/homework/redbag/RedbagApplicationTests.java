aae598797b81a360052ebcea53a6657a376f609d
bai-guangcheng <1010996674@qq.com>
2020-11-24 02:43:23 +0800

    111

---
+++ redbag/src/test/java/com/homework/redbag/RedbagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.redbag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedbagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
