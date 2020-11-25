0cc26ed33f81b1a0527ef7d3ee592a37277dc5b0
bai-guangcheng <1010996674@qq.com>
2020-11-25 14:48:11 +0800

    111

--- redbag/src/main/java/com/homework/redbag/LuckgMoneyController.java
+++ redbag/src/main/java/com/homework/redbag/LuckgMoneyController.java
@@ -2,14 +2,14 @@ package com.homework.redbag;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/bag")
 public class LuckgMoneyController {
 
     @Autowired
@@ -23,11 +23,32 @@ public class LuckgMoneyController {
         return luckyMoneyList;
     }
 
-    public LuckyMoney postRedBag(String producer, BigDecimal money){
+
+    @GetMapping("/save")
+    public LuckyMoney postRedBag(@RequestParam(value = "pro",required = true) String producer,@RequestParam(value = "mon",required = true) BigDecimal money){
         LuckyMoney luckyMoney = new LuckyMoney();
         luckyMoney.setProducer(producer);
         luckyMoney.setMoney(money);
         return repository.save(luckyMoney);
+    }
+
+    @GetMapping("/find/{id}")
+    public LuckyMoney findById(@PathVariable("id") Integer id){
+//        Optional<LuckyMoney> optional = repository.findById(id);
+//        LuckyMoney luckyMoney = optional.get();
+//        return luckyMoney;
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/put/{id}")
+    public LuckyMoney put(@PathVariable("id")Integer id,@RequestParam(value = "consumer",required = true) String consumer){
+        Optional<LuckyMoney> optional = repository.findById(id);
+        if (optional.isPresent()){
+            LuckyMoney luckyMoney = optional.get();
+            luckyMoney.setConsumer(consumer);
+            return repository.save(luckyMoney);
+        }
+        return null;
     }
 
 }
